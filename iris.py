ed276f22eee71a2358cb34298b71c84fbd34fdcf
Uday Chopra <uday.chopra@nagarro.com>
2020-04-13 09:58:01 +0530

    push to github

--- iris.py
+++ iris.py
@@ -11,10 +11,17 @@ if __name__ == '__main__':
     # mlflow.create_experiment("mlflowproject_demo1")
     # mlflow.set_experiment("mlflowproject_demo1")
     X, y = load_iris(return_X_y=True)
-    clf = svm.SVC(kernel='linear', C=1)
+    clf = svm.SVC(kernel='linear', C=10)
     scores = cross_val_score(clf, X, y, cv=5, scoring='f1_macro')
-    mlflow.log_params({'kernel': 'linear', 'C':1})
+    mlflow.log_params({'kernel': 'linear', 'C':10})
     mlflow.log_metrics({"score": scores.mean(), 'score2':scores[0]})
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-    mlflow.sklearn.log_model(clf, "iris classification model")
+    # mlflow.sklearn.log_model(clf, "iris classification model")
     print("Model saved in run %s" % mlflow.active_run().info.run_uuid)
+    mlflow.sklearn.log_model(clf, "SVM_CLF_model")
+
+    # mlflow.sklearn.save_model(clf, 'iris_SVM_clf')
+    # saved this model for mlflow 
+
+
+    # mdl = mlflow.pyfunc.load_model(model_path)
